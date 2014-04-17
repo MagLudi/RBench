1d2af74fdda613d62ab02b2523299ef6b6ac61bc
ljlevin <lawrence.j.levin@gmail.com>
2014-04-17 17:46:17 -0500

    noiseSize tweak

--- BenchmarkHW.R
+++ BenchmarkHW.R
@@ -161,7 +161,7 @@ bmrk.dbscan <- function(size=1, seed=1234){
     library('fpc')
     source('./TestDataGenerator.R', echo=FALSE)  
     
-    noiseSize = 900*size
+    noiseSize = 500*size
     clusterSize = 300*size
     set.seed(seed)
     # noise is same for all so do that upfront....
